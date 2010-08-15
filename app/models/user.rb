7d5e13c7ebef692a80c959a47bd9db2e808f163e
Michael He <hlxwell@gmail.com>
2010-08-15 16:38:39 +0800

    add mini image for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
   attr_accessible :password_confirmation, :avatar, :password, :login, :email, :avatar_file_name, :avatar_content_type, :avatar_file_size, :avatar_updated_at
   acts_as_authentic
-  has_attached_file :avatar, :styles => { :thumb => "40x40#" }
+  has_attached_file :avatar, :styles => { :thumb => "40x40#", :mini => "10x10#" }
 end
